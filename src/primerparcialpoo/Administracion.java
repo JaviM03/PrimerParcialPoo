2bd73783b09f39dec5d5eef8c1775b4e57ddfee9
Carlos Ruiz <00095417@uca.edu.sv>
2018-05-03 15:09:11 -0600

    administracion

---
+++ src/primerparcialpoo/Administracion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerparcialpoo;
+
+/**
+ *
+ * @author Carlos Ruiz
+ */
+public class Administracion {
+
+    public double costoDiaHabitacionS;
+    public double costoDiaHabitacionD;
+    Reserva reserva;
+
+    public Administracion() {
+    }
+
+    public double getCostoDiaHabitacionS() {
+        return costoDiaHabitacionS;
+    }
+
+    public double getCostoDiaHabitacionD() {
+        return costoDiaHabitacionD;
+    }
+
+    public void setCostoDiaHabitacionS(double costoDiaHabitacionS) {
+        this.costoDiaHabitacionS = costoDiaHabitacionS;
+    }
+
+    public void setCostoDiaHabitacionD(double costoDiaHabitacionD) {
+        this.costoDiaHabitacionD = costoDiaHabitacionD;
+    }
+
+}
