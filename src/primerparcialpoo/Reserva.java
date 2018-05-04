a5368271e14d31f262c0d39a1099238db4d6f449
Javier Martinez <00034315@uca.edu.sv>
2018-05-04 00:19:46 -0500

    Se agregó un switch para habitaciones, se puede usar distrihotel con un return

--- src/primerparcialpoo/Reserva.java
+++ src/primerparcialpoo/Reserva.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import java.time.LocalDateTime;
+import java.util.InputMismatchException;
 
 
 
@@ -15,47 +16,88 @@ public class Reserva {
     int reservaMax2H=0, habitacion,dia,ospeMax7D;
     DistriHotel ultimo2Piso,numHabitacion,Distri_hotel;
     String diaReserva;
+    DistriHotel hotel = new DistriHotel();
+    String idReserva;
     
     
+   
     
-    
-    public ArrayList<Reserva> listaReservaHecha= new ArrayList<>();
    
    
     
+    Scanner input = new Scanner (System.in);
     
     //Revisar Disponibilidad de habitaciones
-  
+    public ArrayList<ArrayList<Boolean>> reservaMax2h()
     { 
-        Scanner input = new Scanner (System.in);
-        
+        ArrayList<ArrayList<Boolean>> edificio= hotel.DistriHotel(10, 6);
         System.out.println("Digite el # de habitaciones deseadas: (Maximo 2)");
-        
-        
          while(reservaMax2H<2){
             habitacion= input.nextInt();
-            if( habitacion<3 ){
+            if( habitacion == 2 || habitacion == 1 ){
                 System.out.println("Ha seleccionado " + reservaMax2H + " habitaciones");
-            reservaMax2H=habitacion+1;}
+                reservaMax2H=habitacion+1;}
             else {
-                
                 System.out.println("Ingrese una cantidad valida.");
+            } 
+         }
+         
+        Scanner menu = new Scanner (System.in);
+        boolean salir =false;
+        int opcion;
+          
+        for(int i = 0; i < habitacion; i++ ){
+            System.out.println("1. Habitación Sencilla");
+            System.out.println("2. Habitación Doble");
+            System.out.println("3. Salir");
+            try {   
+            
+                System.out.println("Elige un tipo de Habitación");
+                opcion = menu.nextInt();
+                switch(opcion){
+                    case 1:
+                        System.out.println("Has seleccionado Habitación simple");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado Habitación Doble");
+                        break;
+                     case 3:
+                        salir=true;
+                        break;
+                     default:
+                        System.out.println("Solo los números 1 y 2");
+                }    
+            }    
+            catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                menu.next();
+            }
+        }
+        
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 10; j++){
                 
-            }     
-             
-                        
-               
-                }
+            }
+            
+        }
         
+        return edificio;
+    }       
+    
         
-        
+
         
     
             
    
     //Cantidad de días en los que se reservará la habitación
     
-    { 
+    
+        
+   
+
+    
+        public void ospeMax7d(){
          System.out.println("Digite el # de dias de reserva: (Maximo 1 semana)");
         
          
@@ -77,7 +119,7 @@ public class Reserva {
                
                 }
         
-        
+        }
         
         }
    
@@ -85,8 +127,8 @@ public class Reserva {
         Date diaReserva= new Date(115, 6, 2, 15, 30);
         System.out.println("Ahora hemos creado un objeto date con la fecha indicada, "+diaReserva);*/
 
-    }
-}
+    
+
 
     
     
