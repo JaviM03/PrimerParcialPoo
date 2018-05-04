2ad4dbcd9a808c9b459666082b5fdb3558cd9421
marym <marym@LAPTOP-KN0C8FKQ>
2018-05-04 09:16:11 -0600

    clase factura

--- src/primerparcialpoo/Factura.java
+++ src/primerparcialpoo/Factura.java
@@ -9,6 +9,50 @@ package primerparcialpoo;
  *
  * @author javier
  */
+
 public class Factura {
+    private String idFactura;
+    private double totalPagar;
+    
+    public void Factura(){}
+    
+    public void Factura (String idFactura, double totalPagar){
+        this.idFactura=idFactura;
+        this.totalPagar=totalPagar;
+        
+    }
+    
+    //GETTERS
+
+    public String getIdFactura() {
+        return idFactura;
+    }
+
+    public double getTotalPagar() {
+        return totalPagar;
+    }
+    
+    //SETTERS
+
+    public void setIdFactura(String idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public void setTotalPagar(double totalPagar) {
+        this.totalPagar = totalPagar;
+    }
+    
+    //METODO
+    
+    public void PresentarFactura(){
+        Paquete factura = new Paquete();
+        Factura fac=new Factura();
+        System.out.println("Factura # " + fac.getIdFactura() );
+        System.out.println("nombre cliente: "+factura.getNombre());
+        System.out.println("el costo a pagar es: " + factura.getCosto());
+        System.out.println("el servicio utilizado por el cliente es" + factura.getServicio());
+        System.out.println("Total a pagar: " + fac.getTotalPagar());
+        
+    }
     
 }
