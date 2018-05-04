87dc0d8a4a32ae91e4515460508ccc8bc02ea3a0
Carlos Ruiz <00095417@uca.edu.sv>
2018-05-03 21:33:08 -0600

    parte de la clase administracion, me hace falta varios metodos

--- src/primerparcialpoo/Administracion.java
+++ src/primerparcialpoo/Administracion.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package primerparcialpoo;
+import java.util.Scanner;
 
 /**
  *
@@ -11,9 +12,11 @@ package primerparcialpoo;
  */
 public class Administracion {
 
-    public double costoDiaHabitacionS;
-    public double costoDiaHabitacionD;
-    Reserva reserva;
+    private double costoDiaHabitacionS;
+    private double costoDiaHabitacionD;
+    private DistriHotel distriHotel = new DistriHotel();
+    //private Reserva reserva = new Reserva();
+    private Cliente cliente = new Cliente();
 
     public Administracion() {
     }
@@ -34,4 +37,21 @@ public class Administracion {
         this.costoDiaHabitacionD = costoDiaHabitacionD;
     }
 
+    //-----------------------METODOS------------------------------------------
+    
+    public void agregar(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese nombre:\n");
+        cliente.setNombre(entrada.nextLine());
+        System.out.println("Ingrese apellido:\n");
+        cliente.setApellido(entrada.nextLine());
+        System.out.println("Ingrese telefono:\n");
+        cliente.setTel(entrada.nextLine());
+        System.out.println("Ingrese dui:\n");
+        cliente.setDUI(entrada.nextLine());
+        System.out.println("Ingrese numero de targeta:\n");
+        cliente.setNumeTargeta(entrada.nextLine());
+        
+    }
+    
 }
