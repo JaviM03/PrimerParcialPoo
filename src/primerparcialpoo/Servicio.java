45bf9303578cd9ceac19fd5562ff01f87fd3fa38
marym <marym@LAPTOP-KN0C8FKQ>
2018-05-04 09:17:23 -0600

    clase servicio

--- src/primerparcialpoo/Servicio.java
+++ src/primerparcialpoo/Servicio.java
@@ -10,55 +10,56 @@ package primerparcialpoo;
  * @author marielos
  */
 public class Servicio {
-/*Atributos de la clase Servicio*/
+
+    /*Atributos de la clase Servicio*/
+    private String nombreServicio;
     private String listaServicio;
     private double costo;
     private boolean ilimitado;
-    private String nombre;
 
- /*Creando el Contructor que tiene el mismo nombre de la clase*/
+    /*Creando el Contructor que tiene el mismo nombre de la clase*/
  /*Contructor de Usuario*/
-    public Servicio(){
-        
+    public Servicio() {
     }
-    public Servicio(String listaServicio, double costo, boolean ilimitado, String nombre){
+
+    public Servicio(String nombreServicio, String listaServicio, double costo, boolean ilimitado) {
+        this.nombreServicio = nombreServicio;
         this.listaServicio = listaServicio;
         this.costo = costo;
         this.ilimitado = ilimitado;
-        this.nombre = nombre;
     }
- /*Getters y Setters de la clase Usuario*/
-    public String getListaServicio(){
+
+    /*Getters y Setters de la clase Usuario*/
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    public String getListaServicio() {
         return listaServicio;
     }
-    public double getCosto(){
-        return costo;
-    }
-    public boolean getIlimitado(){
-        return ilimitado;
-    }
-    public String getNombre(){
-        return nombre;
-    }
-    public void setListaServicio(String listaServicio){
+
+    public void setListaServicio(String listaServicio) {
         this.listaServicio = listaServicio;
     }
-    public void setCosto(double costo){
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
         this.costo = costo;
     }
-    public void setIlimitado(boolean ilimitado){
+
+    public boolean isIlimitado() {
+        return ilimitado;
+    }
+
+    public void setIlimitado(boolean ilimitado) {
         this.ilimitado = ilimitado;
     }
-    public void setNombre(String nombre){
-        this.nombre = nombre;
-    } 
-    public void agregarServicio(){
-        
-    }
-    public void eliminarServicio(){
-        
-    }
-    public void modificarServicio(){
-        
-    }
+
 }
